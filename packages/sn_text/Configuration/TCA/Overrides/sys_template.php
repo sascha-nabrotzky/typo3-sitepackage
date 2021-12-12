750e1c2cd85a8b997f124e351e6d5abedcbf8efd
Sascha <68822358+sascha-nabrotzky@users.noreply.github.com>
2021-12-12 11:26:50 +0100

    [TASK] Delete unecessary files

--- packages/sn_text/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Add an entry in the static template list found in sys_templates for static TS
-// on change flush BE
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-'sn_text',
-'Configuration/TypoScript/',
-'Content-Element Text'
-);
