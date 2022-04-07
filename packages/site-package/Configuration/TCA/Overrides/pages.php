f79e50fd874d85fed68adfeb81b780651397ac25
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2022-04-07 20:48:09 +0200

    Set paths for news, set static include

--- packages/site-package/Configuration/TCA/Overrides/pages.php
+++ packages/site-package/Configuration/TCA/Overrides/pages.php
@@ -7,11 +7,17 @@ call_user_func(function()
 
     $extensionKey = 'site-package';
 
+    # Diese Datei (pages.php) meldet import.tsconfig im BE an, dort dann auswählen
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
         $extensionKey,
         'Configuration/TSconfig/import.tsconfig',
         'site-package'
     );
-});
 
-# Diese Datei (pages.php) meldet import.tsconfig im BE an, dort dann auswählen
+    # Add an entry in the static template list found in sys_templates for static TS
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript/*.typoscript',
+        'site-package'
+        );
+});
